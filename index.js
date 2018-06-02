6e41de266f39e32152c77566e13e62c7def67960
Hugh A <harcenea@gmail.com>
2018-06-02 21:13:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var objects = recipes{}var;
-recipes = {};
+var objects = recipes{}
+var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value)
 { var object = recipes;
+var key = 2;
 object.key = value; 
 return object;
   
