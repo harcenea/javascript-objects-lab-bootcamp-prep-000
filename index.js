8409c22fe4f620d42fcbefc09436a694ef347b81
Hugh A <harcenea@gmail.com>
2018-06-02 21:18:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var objects = recipes;
-var recipes = {};
+var recipes = {
+  prop: 1,
+  prop2: 2,
+};
 
 function updateObjectWithKeyAndValue(object, key, value)
 { var object = recipes;
 var key = 2;
 object.key = value; 
 return object;
-  
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+{
+  var object = object.key
 }
